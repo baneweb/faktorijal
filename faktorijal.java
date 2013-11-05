b68ee0607a81308f314f5e19ae7be36b2589ea5a
baneweb <baneweb@gmail.com>
2013-11-05 16:58:44 +0100

    Update faktorijal.java

--- faktorijal.java
+++ faktorijal.java
@@ -1,7 +1,7 @@
 public class matematika {
 
    public static void main(String[] args) {
-	   int n = 5;
+       int n = 5;
        int rezultat = 1;
        for (int i = 1; i <= 5 ; i++){
     	   rezultat = rezultat * i;
